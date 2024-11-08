2ff1dbbfeeb04a7c9f22578a784dca949df1b623
hello-world-ttj <tijotjoseph003@gmail.com>
2024-11-08 11:17:23 +0530

    #admin, role, check-access

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,52 +1,82 @@
 const Joi = require("joi");
 
 exports.createEventSchema = Joi.object({
-    eventName: Joi.string().required(),
-    description: Joi.string().required(),
-    type: Joi.string().required(),
-    image: Joi.string(),
-    startDate: Joi.date().required(),
-    startTime: Joi.date().required(),
-    endDate: Joi.date().required(),
-    endTime: Joi.date().required(),
-    platform: Joi.string(),
-    link: Joi.string(),
-    venue: Joi.string(),
-    organiserName: Joi.string().required(),
-    speakers: Joi.array()
-      .items(
-        Joi.object({
-          name: Joi.string().required(),
-          designation: Joi.string().required(),
-          role: Joi.string().required(),
-          image: Joi.string(),
-        })
-      )
-      .required(),
-    status: Joi.string(),
-  });
-  
-  exports.editEventSchema = Joi.object({
-    eventName: Joi.string(),
-    description: Joi.string(),
-    type: Joi.string(),
-    image: Joi.string(),
-    startDate: Joi.date(),
-    startTime: Joi.date(),
-    endDate: Joi.date(),
-    endTime: Joi.date(),
-    platform: Joi.string(),
-    link: Joi.string(),
-    venue: Joi.string(),
-    organiserName: Joi.string(),
-    speakers: Joi.array().items(
+  eventName: Joi.string().required(),
+  description: Joi.string().required(),
+  type: Joi.string().required(),
+  image: Joi.string(),
+  startDate: Joi.date().required(),
+  startTime: Joi.date().required(),
+  endDate: Joi.date().required(),
+  endTime: Joi.date().required(),
+  platform: Joi.string(),
+  link: Joi.string(),
+  venue: Joi.string(),
+  organiserName: Joi.string().required(),
+  speakers: Joi.array()
+    .items(
       Joi.object({
-        name: Joi.string(),
-        designation: Joi.string(),
-        role: Joi.string(),
+        name: Joi.string().required(),
+        designation: Joi.string().required(),
+        role: Joi.string().required(),
         image: Joi.string(),
       })
-    ),
-    status: Joi.string(),
-  });
-  
+    )
+    .required(),
+  status: Joi.string(),
+});
+
+exports.editEventSchema = Joi.object({
+  eventName: Joi.string(),
+  description: Joi.string(),
+  type: Joi.string(),
+  image: Joi.string(),
+  startDate: Joi.date(),
+  startTime: Joi.date(),
+  endDate: Joi.date(),
+  endTime: Joi.date(),
+  platform: Joi.string(),
+  link: Joi.string(),
+  venue: Joi.string(),
+  organiserName: Joi.string(),
+  speakers: Joi.array().items(
+    Joi.object({
+      name: Joi.string(),
+      designation: Joi.string(),
+      role: Joi.string(),
+      image: Joi.string(),
+    })
+  ),
+  status: Joi.string(),
+});
+
+exports.createRoleSchema = Joi.object({
+  roleName: Joi.string().required(),
+  description: Joi.string(),
+  permissions: Joi.array(),
+  status: Joi.boolean(),
+});
+
+exports.editRoleSchema = Joi.object({
+  roleName: Joi.string(),
+  description: Joi.string(),
+  permissions: Joi.array(),
+  status: Joi.boolean(),
+});
+
+exports.createAdminSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  password: Joi.string().required(),
+  role: Joi.string().required(),
+  status: Joi.boolean(),
+});
+
+exports.editAdminSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  role: Joi.string(),
+  status: Joi.boolean(),
+});
