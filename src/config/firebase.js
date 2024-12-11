56c52945480d5097a1ec2966e0f2a94dc428bc5f
hello-world-ttj <tijotjoseph003@gmail.com>
2024-12-11 16:51:56 +0530

    #service

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,16 @@
 exports.serviceAccount = {
   type: "service_account",
-  project_id: "hef-app-b1e5f",
-  private_key_id: "06894a307d162744f856be31ba62fd5880b4ddc5",
+  project_id: "hef-app-f825a",
+  private_key_id: "5089a5bd7b211758fa6cde3c56d42d56d67f89fd",
   private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQD/0Gq7mJKgpP4t\nSfxofmpHFbLBZR2FvKzujGHKtzC8zuRclrEQJaFm2prNPtKOyXgfP25CdKh5lwfU\nLl/koe4007hQIBkGRdDUKP/JaeoWMP15lmqb3dZA/cM+Fndoybrt5NLE7sKOxQW5\nVuv3VOxuSzRtSAwjek23ikOdHJr80GeyNBLTezZBoLtKX3qtji2gKXTrs0o0GqJJ\nCULmyrxF+XTEA/oCSnbKa8JETXmFwAfNcF/ik7yKekHzqZvzte27Q+D7qQWeuD0m\niJHiQ1mwvBl2C8sPveUsTFJNvXs8UiOTzExgL3aBJB09uaTMeycO8tBrUFYgjz5r\nn8XC+8aRAgMBAAECggEACxlkYCJVDlJKUvW4VWJSwyXOXTTBbKmMiUcfARgkrbR+\nVQQWx6ASWi+W8tvMb/SO2TA73PdJzgi/BrZCY92gw7sB7rwNyedikan7uyX0yaSz\ngsCDfKLJ5oT8ISgBY1MJYMQCXxgK6f3jazC5yT/uiDSMIEe1ex6V26ZMm7b/hIOC\nH1pdNY/QzABQ1pcdwnDpuAYeK54BYAsPEaswNyns+/ZVW0OExPYFr7H9C8qNPrMg\n0b7R8Ru/6M3aClvVQBCUILiVldJoFLVvviD2ZNDt8Zu9BsheqKFcNNNRqb0GmCGD\nUyJi4fyM+x7nLnMA1Z/P7Wi+yZxOeDJ3SU/CKQ3LSQKBgQD/8amyu+3nJvH/DVf0\nxZXuaFnKP76PKTD1/WesVrrkEu3bFjayB8RF2J0rI6Bn95PWms4z8drcMlVDtuT2\nMIEq5nzqm/bQUi8DEs6PCgLclEN2OygKRLnckVnQ42f4hteQCwgoNP7T7VQVJyx1\n2L/QVU+xxGM+W3bAhS/i5mcgNQKBgQD/3r8sGz2wto8mNkf/fIDl3niio0wa9xEv\noOlOr+t1iWaAdjg7049caM+PFv0xIhpo2P8FR2daSPNyFj8rGyK83LnKW440enMg\n/3OPiU/FOMTtAfSBAcCSJ3tOlpyasBE+zYevRr7AC/fML75E6Gt6RFuSwjJdF7Qz\nMgNbkEjQbQKBgQC6xs7CQmbAsykSqdgwJijeDITKFW6o5rR2L3EC0WAdpLqzkShl\nD0nqdsNrgdpDROc8S0iDm9HNs2WJvuSjzI3WktFCHIMnT3JDWewncnTYJ9J7a3wy\n0iiYj/HtgKaVNUIGOia/wLzeR2l+7lGBxqVkEX3RCrIqBXFJ2Wgyu1UyqQKBgESd\n0HiciYulOXhO4Dn0goCii4lZ72Pf090I8ugmJyerBDD5AxWQI4btmJPP33BYq+po\nHuOkhWPh4l869g+yRDuPmcdZjCYonE1CFBRvB1RlZ/IDLzWVabYIHnoxy6d0Bfvo\nlDV97FOGIhrAxu6lwYTVZhVSPL1YSPvdsqbQZ6Z1AoGBAJvSsmPWFYLPxRFrJgZV\nY9PiMzarOHNwfUX/w7fOfifjtWIpztHBAMu+6Vv/niwDYQq/dr6je+E0qQ14SeV+\naYMZB/gnJzXNPYrX1jX+QLPlLV8L/fk+jgF+uPm04VNTs4Y6pLJqZ4iVQGMgEtlf\n3OgYYmHOe51TsV7xlehHwgRk\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-x1gt8@hef-app-b1e5f.iam.gserviceaccount.com",
-  client_id: "117301028562825265173",
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCewlm0ELYUMocH\nRgL9x/9+NDPgOaU/D60Wiej7Mf62PzoYm89szjBJwEtAYLqCQ3KU6Idcc7oLi180\noxNCggsxFOgR45hUU07QN/rGtv6XxnaxV3DQ5B9QwRklqNYBeW/hnHnwM/G03OPW\nhnW5KqIzftBWmJK8rqk59MTWqOkC5UxCEGNd8FrPLfDaBYXblLKaTyRoyjb7/IiJ\n+jUpFV5QjdHTzrrENoNQuscBY4e7y/YV/CYjZGw6Zlb/5hqAFXLH+EayglCshvZ7\nprp3bmdwOiQ63ICN6As2qZSGpAFckuovjUQJFyiNTd/FGS/tBQZNFbuTOxL35sVY\niZtSoqJHAgMBAAECggEAKhDLHfgTlrRwrM+zOTVGEFWarT34+AWaUk+GaRCtMfL8\n3MpkIqtbhh8qkUtP//YJJUWboDaVxDtQpazUDJpShtXIS/0nPqi4SFmc3Axe1y5i\nAsltfhwD5CzWEfFziv53U6tXb/sDf23L/OqxxLd0NPrUDy3sK4yIEHA3u70TyI04\nV+GQPRVVflZtvDak7vFpYst5dpp9ddRRfGLQblkcKi20PO9Nhj8OVPkCYR9XxKlL\nSHzrTkd3mXNq5eW+8jD0HuDZ6v5mFgRW3X0K19gBXMrhFrmnUCIENcBEJAfmI03b\nRQva/JZh7PfLk4W+rz8QmZFi156o+dSTAAx9DbuTuQKBgQDMhb6nSTffEiZyviDR\nYwo2E2Acu9eX0xGOiih9HkDUFeFKaOf2l/0H1fVxXjTEPoEWApUHcJCr+BdIiJEP\nJzYPU2rFT5ZWZcrdfNnO/j5TP60y33v6Z2tH343qFmtqI+FLYUVOACx2FFGaAtwt\nIYF7/k+3V6yVFdPhuz+GjTxnAwKBgQDGt+Iar/4pZIwRq34U6ARiuZrkUoiuwKJW\nLCjdA3sIKxumeF4v16d3UmL7xDWimWt8b5GUraBgOpks9puS44aAqYl4ElIriffV\nzW4MWbNTlaKKwaIn+s0S87zrKtujVi2yorc8Rn1rcGURRX+/Ch0ChZXoB9GH6lYK\n89HIUHRCbQKBgEmYix2pdEh6QImjry2JxR/mYvyT/6WrlzLCtoUngM6QvQxIqcgL\ns0rBjFvqOXSwxUTFUxllQToQwVghhIl+Y66qJs6vr/qREKoZBku9I3JThKC82OEn\nm+w2HtwwiyCbnrPAYe8KkvU+pIqWIuK+ntiej9L64qiXPTKKbfaeFjE/AoGAYkym\nEXOLs6gkTb1agjb/rGKKB3X4XRG3fA8eK65EFxdT9zZFyT7RfYoIoug9a/CephB+\noGB/G/sAuKhI7xGUAGNdOEM/2H/IVD/ptzxMJAW6QQ9mCobbGQvvIRmzSoof0uUA\n5VUtR49yv2d0uOjkztGMBRqRAL3f0E0qMf/ycsUCgYEAlaqCWf6Gg3PmVpKz+m7+\nK3As5Y94Njr9Js8WrnY4TPhpIDm0qUusgNk08rZ1YiDhxdX3Ukye+NL16rX2qKmy\nHd6zAvyDRu4bKv/5Tiy3tXGUZAR0NXxftFmIN67Y9ehIoBUpS9QwYBF8KVVlxonM\nKTBa1ilHjXaYTwH2yyBhM94=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-s8n90@hef-app-f825a.iam.gserviceaccount.com",
+  client_id: "108165762052498764634",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-x1gt8%40hef-app-b1e5f.iam.gserviceaccount.com",
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-s8n90%40hef-app-f825a.iam.gserviceaccount.com",
   universe_domain: "googleapis.com",
 };
+  
