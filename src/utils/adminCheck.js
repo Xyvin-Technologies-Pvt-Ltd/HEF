00e7eabd38b1031b8a61669cca5f35326f11c75f
hello-world-ttj <tijotjoseph003@gmail.com>
2025-01-22 13:11:05 +0530

    #check-admin

--- src/utils/adminCheck.js
+++ src/utils/adminCheck.js
@@ -5,26 +5,30 @@ const Zone = require("../models/zoneModel");
 
 const isUserAdmin = async (userId) => {
   try {
-    const stateAdmin = await State.findOne({ admins: userId }).select("name");
+    const stateAdmin = await State.findOne({ "admins.user": userId }).select(
+      "name"
+    );
     if (stateAdmin) {
       return { type: "State Admin", name: stateAdmin.name };
     }
 
-    const zoneAdmin = await Zone.findOne({ admins: userId }).select("name");
+    const zoneAdmin = await Zone.findOne({ "admins.user": userId }).select(
+      "name"
+    );
     if (zoneAdmin) {
       return { type: "Zone Admin", name: zoneAdmin.name };
     }
 
-    const districtAdmin = await District.findOne({ admins: userId }).select(
-      "name"
-    );
+    const districtAdmin = await District.findOne({
+      "admins.user": userId,
+    }).select("name");
     if (districtAdmin) {
       return { type: "District Admin", name: districtAdmin.name };
     }
 
-    const chapterAdmin = await Chapter.findOne({ admins: userId }).select(
-      "name"
-    );
+    const chapterAdmin = await Chapter.findOne({
+      "admins.user": userId,
+    }).select("name");
     if (chapterAdmin) {
       return { type: "Chapter Admin", name: chapterAdmin.name };
     }
