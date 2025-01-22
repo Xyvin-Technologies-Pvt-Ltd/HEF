fc1e83056c59b4c32fcb2eb77fd4a8f027fe9810
hello-world-ttj <tijotjoseph003@gmail.com>
2025-01-22 14:50:02 +0530

    #schema

--- src/models/chapterModel.js
+++ src/models/chapterModel.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
-
-const adminSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  role: {
-    type: String,
-    enum: ["president", "secretary", "treasurer"],
-  },
-});
+const adminSchema = require("./adminSchema");
 
 const chapterSchema = new mongoose.Schema(
   {
