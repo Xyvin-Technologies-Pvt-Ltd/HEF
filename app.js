c40aeb4154fbb9b4b1237052806c01ce0d194a01
hello-world-ttj <tijotjoseph003@gmail.com>
2025-03-12 17:09:46 +0530

    #expiryDate

--- app.js
+++ app.js
@@ -135,6 +135,8 @@ app.all("*", (req, res) => {
   return responseHandler(res, 404, "No API Found..!");
 });
 
+console.log("Checking server");
+
 //! Start the server and listen on the specified port from environment variable
 server.listen(PORT, () => {
   const portMessage = clc.redBright(`✓ App is running on port: ${PORT}`);
