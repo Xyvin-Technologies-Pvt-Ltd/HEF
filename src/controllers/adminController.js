3059fb0620450a25187e7aeecd058d8fdab47cff
Your Name <you@example.com>
2025-10-17 16:05:48 +0530

    admin-search,memberdownload

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -373,7 +373,7 @@ exports.deleteAdmin = async (req, res) => {
 
 exports.fetchLogActivity = async (req, res) => {
   try {
-    const { page = 1, limit = 10, date, status, method, search } = req.query;
+    const { pageNo = 1, limit = 10, date, status, method, search } = req.query;
 
     const filter = {};
 
@@ -384,14 +384,15 @@ exports.fetchLogActivity = async (req, res) => {
     if (status) {
       filter.status = status;
     }
-    if (method || (search && search.trim() !== "")) {
-      filter.httpMethod = {};
-      if (method) filter.httpMethod.$eq = method;
-      if (search && search.trim() !== "") filter.httpMethod.$regex = search.trim();
-      if (search && search.trim() !== "") filter.httpMethod.$options = "i";
+
+    if (method) {
+      filter.httpMethod = method;
+    }
+    if (search && search.trim() !== "") {
+      filter.endpoint = { $regex: search.trim(), $options: "i" };
     }
 
-    const skipCount = 10 * (page - 1);
+    const skipCount = 10 * (pageNo - 1);
 
     const logs = await logActivity
       .find(filter)
@@ -877,9 +878,7 @@ exports.downloadUser = async (req, res) => {
       { header: "Email", key: "Email" },
       { header: "Chapter Name", key: "ChapterName" },
       { header: "Date of Joining", key: "DateOfJoining" },
-      { header: "Address", key: "Address" },
       { header: "Business Catogary", key: "BusinessCatogary" },
-      { header: "Business Sub Catogary", key: "BusinessSubCatogary" },
       { header: "Subscription", key: "Subscription" },
     ];
 
